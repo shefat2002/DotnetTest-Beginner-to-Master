ed33dafd87d884f8bc9e20938278bb34f1ce8b52
Md Shefat Al Mahmud <shefat2002@gmail.com>
2025-12-17 12:23:49 +0600

    Modified Calculate.cs

--- Calculator/Calculate.cs
+++ Calculator/Calculate.cs
@@ -4,7 +4,7 @@ public class Calculate
 {
     public int Add(int a, int b) => a + b;
     public int Subtract(int a, int b) => a - b;
-    public int Multiply(int a, int b) => a % b;
+    public int Multiply(int a, int b) => a * b;
     public double Divide(int a, int b)
     {
         if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
