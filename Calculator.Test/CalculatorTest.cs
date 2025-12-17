02de9a467e3be74bec293f9c89f654e2c61357a6
Md Shefat Al Mahmud <shefat2002@gmail.com>
2025-12-17 17:14:09 +0600

    updated the test file with test by attribute method

--- Calculator.Test/CalculatorTest.cs
+++ Calculator.Test/CalculatorTest.cs
@@ -1,34 +1,50 @@
+using Xunit;
+
 namespace Calculator.Test;
 
 public class CalculatorTest
 {
     private readonly Calculate _calculator = new Calculate();
 
-    [Fact]
-    public void Add_ReturnsCorrectSum()
+    [Theory]
+    [InlineData(2, 3, 5)]
+    [InlineData(10, 20, 30)]
+    [InlineData(-5, 5, 0)]
+    public void Add_ReturnsCorrectSum(int a, int b, int expected)
     {
-        var result = _calculator.Add(2, 3);
-        Assert.Equal(5, result);
+        var result = _calculator.Add(a, b);
+        Assert.Equal(expected, result);
     }
 
-    [Fact]
-    public void Subtract_ReturnsCorrectDifference()
+    [Theory]
+    [InlineData(5, 3, 2)]
+    [InlineData(10, 20, -10)]
+    [InlineData(10, 0, 10)]
+    public void Subtract_ReturnsCorrectDifference(int a, int b, int expected)
     {
-        var result = _calculator.Subtract(5, 3);
-        Assert.Equal(2, result);
+        var result = _calculator.Subtract(a, b);
+        Assert.Equal(expected, result);
     }
 
-    [Fact]
-    public void Multiply_ReturnsCorrectProduct()
+    [Theory]
+    [InlineData(2, 3, 6)]
+    [InlineData(5, -2, -10)]
+    [InlineData(0, 100, 0)]
+    public void Multiply_ReturnsCorrectProduct(int a, int b, int expected)
     {
-        var result = _calculator.Multiply(2, 3);
-        Assert.Equal(6, result);
+        var result = _calculator.Multiply(a, b);
+        Assert.Equal(expected, result);
     }
 
-    [Fact]
-    public void Divide_ReturnsCorrectQuotient()
+    [Theory]
+    [InlineData(6, 3, 2)]
+    [InlineData(10, 2, 5)]
+    // Note: Be careful with integer division if your calculator uses ints
+    // 5 / 2 might return 2 if integer math is used
+    [InlineData(20, 5, 4)]
+    public void Divide_ReturnsCorrectQuotient(int a, int b, int expected)
     {
-        var result = _calculator.Divide(6, 3);
-        Assert.Equal(2, result);
+        var result = _calculator.Divide(a, b);
+        Assert.Equal(expected, result);
     }
 }
